d7eef068e032a7e0be2961a2f529c72e20c1bc35
arshia_sir <98395557+arshiasir@users.noreply.github.com>
2025-11-13 07:13:40 +0330

    Update Next.js configuration to support dynamic base path and enhance GitHub Actions workflow
    - Added dynamic base path configuration based on environment variables.
    - Created a .nojekyll file in the output directory to prevent Jekyll processing during deployment.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const basePath = process.env.BASE_PATH || (isProd ? '/Ashraf' : '')
+
 const nextConfig = {
   output: 'export',
+  basePath: basePath,
   trailingSlash: true,
   images: {
     unoptimized: true,
